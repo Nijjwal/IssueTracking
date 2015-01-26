e371e2b3da5e3d96ec6f59fd51d60fa59a238992
Nijjwal <nijjwal.shrestha@gamil.com>
2015-01-26 01:41:05 -0600

    Added Issues.java Model class that tracks Project Issues

---
+++ src/com/nijjwal/hibernate/Issues.java
@@ -0,0 +1,66 @@
+package com.nijjwal.hibernate;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table (name = "Project_Issues")
+public class Issues {
+
+	
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private int project_id;
+	
+	@Column(name="Project_Title", nullable=false)
+	private String project_title;
+	
+	@Column(name="Issue_Description", nullable=false)
+	private String issue_description;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="Issue_Entered_On", nullable=false)
+	private Date issue_entered_on;
+	
+
+	public int getProject_id() {
+		return project_id;
+	}
+
+	public void setProject_id(int project_id) {
+		this.project_id = project_id;
+	}
+
+	public String getProject_title() {
+		return project_title;
+	}
+
+	public void setProject_title(String project_title) {
+		this.project_title = project_title;
+	}
+
+	public String getIssue_description() {
+		return issue_description;
+	}
+
+	public void setIssue_description(String issue_description) {
+		this.issue_description = issue_description;
+	}
+
+	public Date getIssue_entered_on() {
+		return issue_entered_on;
+	}
+
+	public void setIssue_entered_on(Date issue_entered_on) {
+		this.issue_entered_on = issue_entered_on;
+	}
+	
+	
+}
--- src/com/nijjwal/hibernate/Main.java
+++ src/com/nijjwal/hibernate/Main.java
@@ -9,31 +9,18 @@ import org.hibernate.cfg.AnnotationConfiguration;
 @SuppressWarnings("deprecation")
 public class Main {
 	public static void main(String[] args) {
-
-		Student student = new Student();
-		student.setName("Monster123");
-		student.setBirthDate(new Date());
-		
-		
-		
-		StudentDetail studentDetail = new StudentDetail();
-		studentDetail.setStudent_mobile_number("419-285-xxxx");
-		
-		//this is an important step
-		studentDetail.setStudent(student);
-		
-
-		
+	
+		Issues myissue = new Issues();
+			
 		SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();	
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
+				
+		//Retrieve the object
+		myissue = (Issues) session.get(Issues.class, 1);
+		myissue.setProject_title("Fatigue");
 		
-		
-		//Please note I am not saving student object but still it will be saved in database
-		//that's the magic of one to one mapping
-		session.save(studentDetail);
-
-		
+		session.delete(myissue);
 		
 		session.getTransaction().commit();
 		session.close();
